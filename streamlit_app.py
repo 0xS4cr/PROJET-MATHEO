cd52e290e88709689bb35342a6ab18a725eb0dac
FONTAINE Matheo <matheo.fontaine@interval.coop>
2026-06-12 10:59:10 +0100

    Style fonctionnelle, commencement du deuxième tableau

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,11 @@ with open('./style.css') as f:
     
 st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
 
+#style
+st.set_page_config(
+    layout="wide"
+)
+
 
 #Titre page
 
@@ -167,7 +172,9 @@ elif option == "Par technicien":
             color_evolution,
             subset=["evolution_%"]
         ),
-        hide_index=True
+        hide_index=True,
+        use_container_width=True,
+        height=600
     )
 
 # Fermeture connexion
@@ -178,3 +185,114 @@ elif option == "Par technicien":
 
 elif option == "Par années":
     st.write(''':rainbow[:construction: EN CONSTRUCTION :construction:]''')
+
+
+    cursor = conn.cursor()
+
+    cursor.execute(f"""
+        SELECT *
+        FROM {dbschema}.{dbtable}
+    """)
+
+    df = pd.DataFrame(
+        cursor.fetchall(),
+        columns=[desc[0] for desc in cursor.description]
+    )
+
+    cursor.close()
+
+
+# filtres campagnes
+    
+    df = df[df["campagne_appro"].isin(["2024-2025", "2025-2026"])]
+
+    df["quantite"] = pd.to_numeric(df["quantite"], errors="coerce")
+
+    df_work = df.copy()
+
+
+# sans filtre
+
+    if filtre == "Aucun":
+
+        df_table = pd.pivot_table(
+            df_work,
+            index=["technicien", "code_famille"],
+            columns="campagne_appro",
+            values="quantite",
+            aggfunc="sum",
+            fill_value=0
+        )
+
+
+# filtre
+    else:
+
+        code = filtre.replace("famille n°", "")
+
+        df_work = df_work[
+            df_work["code_famille"].astype(str) == code
+        ]
+
+        df_table = pd.pivot_table(
+            df_work,
+            index=["technicien", "famille_2"],
+            columns="campagne_appro",
+            values="quantite",
+            aggfunc="sum",
+            fill_value=0
+        )
+
+
+# evolution tech
+
+    if "2024-2025" not in df_table.columns:
+        df_table["2024-2025"] = 0
+    if "2025-2026" not in df_table.columns:
+        df_table["2025-2026"] = 0
+
+    df_table["evolution_%"] = (
+        (df_table["2025-2026"] - df_table["2024-2025"])
+        / df_table["2024-2025"].replace(0, float("nan"))
+    ) * 100
+
+
+# Ajouter couleur
+
+    def color_evolution(val):
+        if pd.isna(val):
+            return ""
+        elif val > 0:
+            return "color: green"
+        elif val < 0:
+            return "color: red"
+        else:
+            return ""
+
+
+# AFFICHAGE
+
+    df_table = df_table.reset_index()
+
+    df_table["technicien"] = df_table["technicien"].mask(
+        df_table["technicien"].duplicated()
+    )
+
+    st.dataframe(
+        df_table.style
+        .format({
+            "2024-2025": "{:.2f}",
+            "2025-2026": "{:.2f}",
+            "evolution_%": "{:.2f}%"
+        })
+        .map(
+            color_evolution,
+            subset=["evolution_%"]
+        ),
+        hide_index=True,
+        use_container_width=True,
+        height=600
+    )
+
+# Fermeture connexion
+    conn.close()
